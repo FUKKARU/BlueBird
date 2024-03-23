07992c208ababb2169510fc932f62ab7a012f750
KAKAKAKAKAKAKA <133581820+kakakaka777@users.noreply.github.com>
2024-03-24 00:56:33 +0900

    Timer＆SceneChange(一部）
    ・PawnBattleとBossBattleのTimer追加
    ・PawnBattle）
    CountDown＝前半の時間
    Second...　＝後半の時間
    ・BossBattle
    上記を参考して
    ・Scene遷移（時間が終わり次第）
    PawnB→PawnTalk
    BossB→BossTalk

---
+++ Assets/Scripts/Battle/BossBattle/TImeLimitForBoss.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+
+public class TImeLimitForBoss : MonoBehaviour
+{
+    [SerializeField]
+    private float CountDown = 120f;
+    [SerializeField]
+    private string SceanName;
+    [SerializeField]
+    private TextMeshProUGUI TimeOverText;
+    
+
+
+
+
+    void Start()
+    {
+       
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //時間をカウントダウンする
+        CountDown -= Time.deltaTime;
+
+        //時間を表示する
+        TimeOverText.text = CountDown.ToString("f0") + "秒";
+
+        //countdownが0以下になったとき
+        if (CountDown <= 0)
+        {
+
+
+            SceneManager.LoadScene(SceanName);
+
+
+            
+        }
+    }
+}
