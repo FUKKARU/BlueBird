b44a91e0b25dd130e844ee1324bd22910d3a9c0d
Ajun5GIT <153270000+Ajun5GIT@users.noreply.github.com>
2024-03-24 18:19:21 +0900

    時間修正

--- Assets/Scripts/Title/Title.cs
+++ Assets/Scripts/Title/Title.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour
 {
+    private void Start()
+    {
+        Time.timeScale = 1.0f;
+    }
     public void Play()
     {
         SceneManager.LoadScene("StartTalk");
